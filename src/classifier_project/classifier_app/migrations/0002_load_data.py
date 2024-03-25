1af72e97cbba01bfc30a0d14eb29d34c422740c5
Dan Lincoln <dlinc12@wgu.edu>
2024-03-25 13:18:26 -0700

    Add multiprocess pool during data load.
    Should reduce the time it takes to fully preprocess data from source.

--- src/classifier_project/classifier_app/migrations/0002_load_data.py
+++ src/classifier_project/classifier_app/migrations/0002_load_data.py
@@ -4,47 +4,93 @@
 from ..classifier import preprocess
 from django.db import migrations
 import logging
+import multiprocessing
 import pandas
 
+logging.basicConfig(
+    format="%(asctime)s [%(name)s] %(levelname)s - %(message)s",
+    level=logging.INFO
+)
 logger = logging.getLogger("LoadData")
 
 csvfile = "/data/train.csv"
 
+num_processors = 4
+
+
+def process_data(
+    unprocessed_queue: multiprocessing.Queue,
+    processed_queue: multiprocessing.Queue,
+):
+    while True:
+        text = unprocessed_queue.get()
+        if text is None:
+            processed_queue.put(None)
+            break
+        text.processed = preprocess(text.unprocessed)
+        processed_queue.put(text)
+
+
+def save_data(processed_queue: multiprocessing.Queue):
+    count_done = 0
+    while True:
+        text = processed_queue.get()
+        if text is None:
+            count_done += 1
+            if count_done == num_processors:
+                break
+        else:
+            text.save()
+            text.humanrating.save()
+            text.humanrating.humanratingclass.save()
+
 
 def load_data(apps, schema_editor):
-    """If there are no records in the database yet, load from CSV."""
+    """Load records from CSV"""
     Text = apps.get_model("classifier_app", "Text")
+    if Text.objects.count() > 0:
+        return  # Do not load if the database has records.
+
     HumanRating = apps.get_model("classifier_app", "HumanRating")
     HumanRatingClass = apps.get_model("classifier_app", "HumanRatingClass")
-    if Text.objects.count() == 0:
-        logger.info("Loading data...")
-        df = pandas.read_csv(csvfile)
-        length = len(df)
-        for index, row in df.iterrows():
-            text = Text(
-                id=row["id"],
-                unprocessed=row["comment_text"],
-                processed=preprocess(row["comment_text"]),
-            )
-            human_rating = HumanRating(
-                text=text,
-                raw_rating=row["target"],
-                rating=row["target"] >= 0.5,
-            )
-            human_rating_class = HumanRatingClass(
-                human_rating=human_rating,
-                severe_toxicity=row["severe_toxicity"],
-                obscene=row["obscene"],
-                threat=row["threat"],
-                insult=row["insult"],
-                identity_attack=row["identity_attack"],
-                sexual_explicit=row["sexual_explicit"]
-            )
-            text.save()
-            human_rating.save()
-            human_rating_class.save()
-            if index % 1001 == 0:
-                logger.info(f"Loading data: {index/length:6.2%}")
+
+    unprocessed_queue = multiprocessing.Queue(maxsize=20)
+    processed_queue = multiprocessing.Queue(maxsize=20)
+
+    processor_pool = multiprocessing.Pool(
+        num_processors, process_data, (unprocessed_queue, processed_queue)
+    )
+    saver_pool = multiprocessing.Pool(1, save_data, (processed_queue,))
+
+    df = pandas.read_csv(csvfile)
+    length = len(df)
+    for i, series in df.iterrows():
+        text = Text(id=series["id"], unprocessed=series["comment_text"])
+        text.humanrating = HumanRating(
+            text=text,
+            raw_rating=series["target"],
+            rating=series["target"] >= 0.5,
+        )
+        text.humanrating.humanratingclass = HumanRatingClass(
+            severe_toxicity=series["severe_toxicity"],
+            obscene=series["obscene"],
+            threat=series["threat"],
+            insult=series["insult"],
+            identity_attack=series["identity_attack"],
+            sexual_explicit=series["sexual_explicit"]
+        )
+        unprocessed_queue.put(text)
+        if i % 1000 == 0:
+            logger.info(f"Loading data: {i/length:6.2%}")
+
+    for _ in range(num_processors):
+        unprocessed_queue.put(None)
+
+    processor_pool.close()
+    processor_pool.join()
+
+    saver_pool.close()
+    saver_pool.join()
 
 
 class Migration(migrations.Migration):
@@ -53,4 +99,6 @@ class Migration(migrations.Migration):
         ("classifier_app", "0001_initial"),
     ]
 
-    operations = [migrations.RunPython(load_data)]
+    operations = [
+        migrations.RunPython(load_data)
+    ]
